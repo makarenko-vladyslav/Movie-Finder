4610e39c9736e2f0c5877e7ee58f4a686a677cb7
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-04-29 21:15:22 +0200

    fuuuh

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -5,31 +5,31 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 
 export default function MoviesPage() {
   const [movies, setMovies] = useState([]);
+  const [title, setTitle] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  async function fetchMovies() {
-  try {
-    setLoading(true);
-    const data = await searchMovies(); // Викликайте вашу функцію для пошуку фільмів
-    setMovies(data);
-  } catch (error) {
-    setError(true);
-  } finally {
-    setLoading(false);
-  }
-}
+  useEffect(() => {
+    async function fetchMovies(newTopic) {
+      setMovies([]);
+      try {
+        setLoading(true);
+        const data = await searchMovies(newTopic);
+        setMovies(data);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
 
-  const handleMovies = (newTopic) => {
-    setMovies([]);
-    // Викликайте функцію fetchMovies() при кліку на кнопку пошуку
-    fetchMovies();
-  };
+    fetchMovies(title);
+  }, [title]);
 
   return (
     <div>
       <p>Movies page</p>
-      <SearchBar onSubmit={handleMovies}></SearchBar>
+      <SearchBar onSubmit={setTitle}></SearchBar>
     </div>
   );
 }
